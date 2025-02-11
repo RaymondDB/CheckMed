e084dd4efc27f63b861f6688451903b697378e04
GregCuevas <gregorycuevas12@icloud.com>
2025-02-10 20:17:48 -0600

    feature-tailwind-v4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">FRONTEND!</h1>
+      <Button>REYMAND</Button>
     </>
   );
 };
